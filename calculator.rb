4d83843b690259e07ee51cdc630faa0555433fe0
Mario Visic <mario@mariovisic.com>
2024-01-22 08:58:59 +1100

    Use a switch case statement for the interest paid method check
    I think visually it's a little nicer but doesn't change the complexity
    of the code at all.

--- calculator.rb
+++ calculator.rb
@@ -11,22 +11,23 @@ class Calculator
   def calculate
     balance = @principle
 
-    if @interest_paid == :at_maturity
-      periods_remaining = 1
+    case @interest_paid
+    when :at_maturity
+      periods = 1
       rate = @rate * @years
-    elsif @interest_paid == :annually
-      periods_remaining = @years
+    when :annually
+      periods = @years
       rate = @rate
     else
-      periods_remaining = @years * 12
+      periods = @years * 12
       rate = @rate / BigDecimal(12)
     end
 
-    until periods_remaining <= 0
-      period_interest = (balance * rate / 100 * periods_remaining.clamp(0, 1))
+    until periods <= 0
+      period_interest = (balance * rate / 100 * periods.clamp(0, 1))
       balance += period_interest
 
-      periods_remaining -= 1
+      periods -= 1
     end
 
     balance.round(0)
