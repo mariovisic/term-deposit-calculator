902a40d442c0967cc2808b4be1ca3fac13b4132b
Mario Visic <mario@mariovisic.com>
2024-01-22 08:53:59 +1100

    Refactor the at maturity calculations
    For the at maturity deposit calculations we can use the same code
    pathway as annually and monthly. Also remove the unused period_interest
    local variable.

--- calculator.rb
+++ calculator.rb
@@ -9,27 +9,24 @@ class Calculator
   end
 
   def calculate
+    balance = @principle
+
     if @interest_paid == :at_maturity
-      balance = @principle * (1 + @rate / 100 * @years)
-    elsif @interest_paid == :annually || @interest_paid == :monthly
-      balance = @principle
-      interest_paid = BigDecimal(0)
+      periods_remaining = 1
+      rate = @rate * @years
+    elsif @interest_paid == :annually
+      periods_remaining = @years
+      rate = @rate
+    else
+      periods_remaining = @years * 12
+      rate = @rate / BigDecimal(12)
+    end
 
-      if @interest_paid == :annually
-        periods_remaining = @years
-        rate = @rate
-      else
-        periods_remaining = @years * 12
-        rate = @rate / BigDecimal(12)
-      end
+    until periods_remaining <= 0
+      period_interest = (balance * rate / 100 * periods_remaining.clamp(0, 1))
+      balance += period_interest
 
-      until periods_remaining <= 0
-        period_interest = (balance * rate / 100 * periods_remaining.clamp(0, 1))
-        interest_paid += period_interest
-        balance += period_interest
-
-        periods_remaining -= 1
-      end
+      periods_remaining -= 1
     end
 
     balance.round(0)
